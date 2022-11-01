6116f9cadea728ef09b0b62f7e9ceffdcbf0e9fd
Çağrı <theboztepe@gmail.com>
2022-11-02 00:31:38 +0300

    Add project files.

---
+++ Book.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Book.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ User.Infastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace User.Infastructure
+{
+    public class Class1
+    {
+    }
+}
